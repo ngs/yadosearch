a4c64e548788678e55a88b8561e1e7d2d1f326aa
Atsushi Nagase <a@ngs.io>
2014-09-15 13:25:00 +0900

    wip

---
+++ YadoSearch/YadoSearch-Bridging-Header.h
@@ -0,0 +1,6 @@
+#import <AFNetworking/AFNetworking.h>
+#import <BlocksKit/BlocksKit.h>
+#import <CoreData/CoreData.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import <UIKit/UIKit.h>
